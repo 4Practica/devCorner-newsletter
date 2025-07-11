2380c06950053899ab5ebdcfc056c00489546710
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-07-10 22:43:47 -0400

    ✨ Add environment configuration and email validation utility

---
+++ utils/env.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT,
+  API_KEY: process.env.API_KEY,
+  BREVO_API_KEY: process.env.BREVO_API_KEY,
+  BREVO_TEMPLATE_ID_WELCOME: process.env.BREVO_TEMPLATE_ID_WELCOME,
+  BREVO_TEMPLATE_ID_NEW_POST: process.env.BREVO_TEMPLATE_ID_NEW_POST,
+  FROM_NAME: process.env.FROM_NAME,
+  FROM_EMAIL: process.env.FROM_EMAIL,
+};
