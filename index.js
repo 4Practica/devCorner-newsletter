75108e69b974d07eaa91e8d442e7ba692e5d08fe
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-07-10 23:22:19 -0400

    ✨ Add morgan middleware for logging HTTP requests

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import helmet from 'helmet';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
-import newsletterRoutes from './routes/newsletter.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import { env } from './utils/env.js';
+import newsletterRoutes from "./routes/newsletter.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { env } from "./utils/env.js";
 
 dotenv.config();
 
@@ -14,12 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
 
-app.use('/api/newsletter', newsletterRoutes);
+app.use("/api/newsletter", newsletterRoutes);
 
 app.use(errorHandler);
 
 const PORT = env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Newsletter service running on port ${PORT}`);
+	console.log(`🚀 Newsletter service running on port ${PORT}`);
 });
