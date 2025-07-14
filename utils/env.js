77a64e4a9ce5c0b0961b8739f7a070ae8e0480f5
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-07-14 16:39:21 -0400

    ✨ Parse BREVO_LIST_ID as an integer for consistent type handling

--- utils/env.js
+++ utils/env.js
@@ -9,5 +9,5 @@ export const env = {
 	BREVO_TEMPLATE_ID_NEW_POST: process.env.BREVO_TEMPLATE_ID_NEW_POST,
 	FROM_NAME: process.env.FROM_NAME,
 	FROM_EMAIL: process.env.FROM_EMAIL,
-	BREVO_LIST_ID: process.env.BREVO_LIST_ID,
+	BREVO_LIST_ID: parseInt(process.env.BREVO_LIST_ID),
 };
